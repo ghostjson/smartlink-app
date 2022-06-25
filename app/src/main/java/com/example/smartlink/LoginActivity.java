77f41ea57b3324bb399510acd66181f9efd713f5
Akhil Ravindran <imakhilravindran@gmail.com>
2022-06-25 21:27:46 +0530

    feat: basic application

---
+++ app/src/main/java/com/example/smartlink/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.example.smartlink;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.LoggingBehavior;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Objects;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(LoginActivity.this);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // success
+            }
+
+            @Override
+            public void onCancel() {
+                // cancel
+            }
+
+            @Override
+            public void onError(@NonNull FacebookException e) {
+                // error
+            }
+        });
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Collections.singletonList("public_profile"));
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/com/example/smartlink/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.smartlink;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.facebook.AccessToken;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    String baseURL = "https://main.dmxqa22ma9yp.amplifyapp.com";
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+
+        String accessToken = Objects.requireNonNull(AccessToken.getCurrentAccessToken()).getToken();
+
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl(baseURL);
+
+        CookieManager.getInstance().setCookie(baseURL, "token=" + accessToken + "; path=/");
+
+        webView.setWebViewClient(new WebViewClient());
+    }
+}
